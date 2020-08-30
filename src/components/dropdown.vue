5bcf3cd9671068cfadd1f1cb2064aa3d711dd749
Matej <matejchalachan@gmail.com>
2020-08-30 15:55:40 +0200

    dropdown padding updated

--- src/components/dropdown.vue
+++ src/components/dropdown.vue
@@ -5,7 +5,7 @@
             <select
                 v-model="selectValue"
                 @change="handleOption"
-                class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="width-unit">
+                class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-2 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="width-unit">
                 <option v-for="option in options" :value="option" :key="option"> {{option}}</option>
             </select>
             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
