eea2f4fdb3c1360560ad635fdee88828ad8e73a4
Matej <matejchalachan@gmail.com>
2020-08-31 22:35:03 +0200

    header component

---
+++ src/components/headerComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full h-24 bg-gradient-to-r from-teal-400 to-blue-500 text-white font-semibold mb-4 p-4 flex flex-col justify-center text-2xl shadow-inner"
+         style="text-shadow: 0 15px 30px rgba(0,0,0,0.11), 0 5px 15px rgba(0,0,0,0.08);">
+        Button generator
+    </div>
+</template>
+
+<script>
+export default {
+name: "headerComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
