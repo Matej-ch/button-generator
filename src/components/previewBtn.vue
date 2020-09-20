60a789ea9a0429a31c5933db6aa7c3a35fe415a1
Matej <matejchalachan@gmail.com>
2020-09-20 11:36:00 +0200

    copy functionality closes #7

--- src/components/previewBtn.vue
+++ src/components/previewBtn.vue
@@ -4,12 +4,13 @@
             <div class="border-r border-b border-l border-gray-400 border-t lg:border-gray-400 bg-white rounded p-4 leading-normal w-full max-w-md overflow-auto h-56"
                  style="place-items: center;display: grid;">
 
-                <button :style="btnStyle" ref="output-btn">Button</button>
+                <button :style="btnStyle" ref="outputBtn">Button</button>
             </div>
 
             <div class="flex flex-row py-4 w-full max-w-md justify-between">
                 <a href="#"
                    @click.prevent="copyStyle"
+                   ref="copyBtn"
                    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                     Copy style
                 </a>
@@ -20,6 +21,20 @@
                 </a>
             </div>
 
+            <transition name="fade" mode="out-in">
+                <div v-show="showCode" class="flex flex-row py-4 w-full max-w-md justify-between text-xs" ref="textToCopy">
+                    .btn {{btnStyle}}
+
+                    .btn:hover {
+                        filter: brightness(120%) saturate(120%);
+                    }
+
+                    .btn:active {
+                        filter: saturate(120%);
+                    }
+                </div>
+            </transition>
+
         </div>
     </div>
 </template>
@@ -28,22 +43,35 @@
 export default {
 name: "previewBtn",
     props: {btnStyle:Object},
+    data: function () {
+        return {
+            showCode: true
+        }
+    },
     methods: {
         copyStyle() {
-            //var copyText = document.getElementById("myInput");
 
-            /* Select the text field */
-            //copyText.select();
-            //copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+            const selection = window.getSelection();
+            const range = document.createRange();
+            let node = this.$refs.textToCopy;
+            const copyBtn = this.$refs.copyBtn;
 
-            /* Copy the text inside the text field */
-            //document.execCommand("copy");
+            range.selectNodeContents(node);
+            selection.removeAllRanges();
+            selection.addRange(range);
 
-            /* Alert the copied text */
-            //alert("Copied the text: " + copyText.value);
+            document.execCommand('copy');
+            selection.removeAllRanges();
+
+            const original = copyBtn.innerText;
+            copyBtn.innerText = 'Copied!';
+
+            setTimeout(() => {
+                copyBtn.textContent = original;
+            }, 1200);
         },
         showStyle() {
-
+            this.showCode = !this.showCode;
         }
     }
 }
