f52750ca4a3cc8a1ec4e544cb51e423d6affa4cc
Matej <matejchalachan@gmail.com>
2020-09-20 20:50:52 +0200

    adding deployment

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,8 @@ module.exports = {
                 args[0].title = "Css button creator";
                 return args;
             })
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/button-generator/'
+        : '/'
 }
